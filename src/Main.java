9a77b275d24ccffdb0937818441097e6b9d82f62
9831062 <9831062@ceit.local>
2020-04-09 23:27:32 +0430

    n human

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Ask about the number of players
-        System.out.println("PLease enter the number of players. (3-5)");
-        Scanner scn = new Scanner(System.in);
-        int numberOfPlayers = scn.nextInt();
-        while (numberOfPlayers<3 || numberOfPlayers>5) {
-            System.out.println("Please enter a number between 3 and 5.");
-            numberOfPlayers = scn.nextInt();
-        }
-
         // Ask about the game mode
+        Scanner scn = new Scanner(System.in);
         System.out.println("Do you want to play with computer or human?\n1) Computer\n2) Human");
         int gameMode = scn.nextInt();
         while (gameMode<1 || gameMode>2) {
             System.out.println("Please enter a number between 1 and 2.");
             gameMode = scn.nextInt();
+        }
+
+        // Ask about the number of players
+        System.out.println("PLease enter the number of players." + ((gameMode==1)? "(3-5)" : ""));
+        int numberOfPlayers = scn.nextInt();
+        while ((gameMode==1 && (numberOfPlayers<3 || numberOfPlayers>5)) || (gameMode==2 && numberOfPlayers<1)) {
+            System.out.println("Please enter a valid number.");
+            numberOfPlayers = scn.nextInt();
         }
 
         // Run a new game
